e404ac5ace5d11ccc345d6da03f8eec41994b69b
nboyaroca <nboyaroca@gmail.com>
2022-04-25 12:29:09 +0200

    provesJS

--- index.js
+++ index.js
@@ -45,12 +45,43 @@ const menu = [
         price: 4.79,
         image: './images/Pizza-louis.png'
     }
-]
+];
 
+/*<div class="pizza-box" id="pizza">
+<div class="pizza-image-frame"> <!-- marc de la foto -->
+    <img class="pizza-image" src="./images/Pizza-greek.png" alt="Pizza Sicilian">
+</div>
+<div class="carta"> <!-- informació de la pizza -->
+    <div class="pizza-text">
+        <h4 class="namepizza"></h4>
+        <p class="ingredient">Tomato</p>
+        <h3 class="price">$7.90</h3>
+    </div>
+    <button class="addbutton">+</button> <!-- per afegir a la comanda -->
+</div> */
+
+
+
+
+let html=''
+
+
+for (let i = 0; i <menu.length; i++) {
+    html += menu[i].name + menu[i].ingredient + menu[i].price + '<br>';
+}
+
+let app = document.querySelector('#app')
+app.innerHTML = html
+console.log (app)
+
+
+/*
 let pizza = [];
 const divisa = '€';
 const DOMitems = document.querySelector('#items');
 const DOMpizza = document.querySelector('#pizza');
 const DOMtotal = document.querySelector('#total');
 const DOMemptyButton = document.querySelector('#empty-button');
+*/
+
 
